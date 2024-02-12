ce36f9a9119910e78f78ea38502c6ff2eba60a00
unknown <filip.agatic25@gmail.com>
2024-02-12 17:38:54 +0100

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,28 @@
 import instagramIcon from "../icons/instagram-icon.png";
 import facebookIcon from "../icons/facebook-icon.png";
+import editLogo from "../logos/edIT-logo.png";
 
 function Footer() {
     return (
         <div id="footer-element">
-            <section className="logo-section">
-                <img src=""></img>
-            </section>
             <section className="social-media-links-section">
-                <a href="https://www.facebook.com/edit.udruga/" className="sm-link">
-                    <img src={facebookIcon} className="sm-link-icon"></img>
-                </a>
-                <a href="https://www.instagram.com/udruga_edit/" className="sm-link">
-                    <img src={instagramIcon} className="sm-link-icon"></img>
-                </a>
+                <div className="social-media-flexbox">
+                    <a href="https://www.facebook.com/edit.udruga/" className="sm-link">
+                        <img src={facebookIcon} className="sm-link-icon"></img>
+                    </a>
+                    <a href="https://www.instagram.com/udruga_edit/" className="sm-link">
+                        <img src={instagramIcon} className="sm-link-icon"></img>
+                    </a>
+                </div>
             </section>
             <section className="copyright-section">
                 <small>© Udruga edIT</small>
+            </section>
+            <section className="logo-section">
+                <div className="logo-flexbox">
+                    <img src={editLogo} className="edit-logo"></img>
+                    <span>Ekonomsko društvo informacijskih tehnologija</span>
+                </div>
             </section>
         </div>
     )
