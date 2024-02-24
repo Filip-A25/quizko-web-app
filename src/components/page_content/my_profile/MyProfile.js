87d7c3a124d2ab4e1b542d3ce2257cbcf68168ed
Luka_Hat <luka.hat58@gmail.com>
2024-02-24 22:53:37 +0100

    add initial styling and mock data for profile

--- src/components/page_content/my_profile/MyProfile.js
+++ src/components/page_content/my_profile/MyProfile.js
@@ -1,26 +1,74 @@
-import { useRef, useContext, useEffect } from 'react';
-import {MainContext} from "../../../MainContent";
+import { useRef, useContext, useEffect, useState } from "react";
+import { MainContext } from "../../../MainContent";
+
+//import icon
+import icon from "../../../icons/editIcon.svg";
 
 function MyProfile() {
-    const {handleElementReveal} = useContext(MainContext);
-    const revealingElements = useRef([]);
+  const { handleElementReveal } = useContext(MainContext);
+  const revealingElements = useRef([]);
 
-    useEffect(() => {
-        handleElementReveal(revealingElements);
-        window.addEventListener("scroll", handleElementReveal(revealingElements));
+  //profile fields
+  const [email, setEmail] = useState("dummy@dummy.com");
+  const [nickname, setNickname] = useState("efos");
+  const [password, setPassword] = useState("password");
 
-        return () => {
-            window.removeEventListener("scroll", handleElementReveal(revealingElements));
-        }
-    }, [])
+  // logic for hiding password
+  useEffect(() => {
+    const passwordHider = () => {
+      const length = password.length;
+      setPassword("*".repeat(length));
+    };
 
-    return (
-        <div id="my-profile-content">
-            <div className="component-content">
+    passwordHider();
+  }, [password]);
 
-            </div>
+  useEffect(() => {
+    handleElementReveal(revealingElements);
+    window.addEventListener("scroll", handleElementReveal(revealingElements));
+
+    return () => {
+      window.removeEventListener(
+        "scroll",
+        handleElementReveal(revealingElements)
+      );
+    };
+  }, [handleElementReveal]);
+
+  return (
+    <div id="my-profile-content">
+      <div className="component-content">
+        <h1 className="mp-heading">Moj profil</h1>
+        <div className="mp-fields">
+          <div className="mp-fields-field">
+            <h3 className="mp-field-heading">E-mail</h3>
+            <p className="mp-field-data">{email}</p>
+            <button className="mp-field-button">
+              <img src={icon} alt="uredi" />
+              <span>Uredi</span>
+            </button>
+          </div>
+          <div className="mp-fields-field">
+            <h3 className="mp-field-heading">Nadimak</h3>
+            <p className="mp-field-data">{nickname}</p>
+            <button className="mp-field-button">
+              <img src={icon} alt="uredi" />
+              <span>Uredi</span>
+            </button>
+          </div>
+          <div className="mp-fields-field">
+            <h3 className="mp-field-heading">Lozinka</h3>
+            <p className="mp-field-data">{password}</p>
+            <button className="mp-field-button">
+              <img src={icon} alt="uredi" />
+              <span>Uredi</span>
+            </button>
+          </div>
         </div>
-    )
+        <button className="mp-delete-button">Izbriši profil</button>
+      </div>
+    </div>
+  );
 }
 
 export default MyProfile;
