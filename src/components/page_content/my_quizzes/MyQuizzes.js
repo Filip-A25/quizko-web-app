e0f9a2a2ddaa19de1f6b76631be24608fe39114e
Luka_Hat <luka.hat58@gmail.com>
2024-05-07 20:56:12 +0200

    add filters for quizzes based on category

--- src/components/page_content/my_quizzes/MyQuizzes.js
+++ src/components/page_content/my_quizzes/MyQuizzes.js
@@ -6,10 +6,14 @@ import { Link } from "react-router-dom";
 
 import arrowLeft from "../../../icons/arrow-left.svg";
 import arrowRight from "../../../icons/arrow-right.svg";
+import { getAllCategories } from "../../../services/api_categories";
 
 function MyQuizzes() {
   const { handleElementReveal } = useContext(MainContext);
   const [quizzes, setQuizzes] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [originalQuizzes, setOriginalQuizzes] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
   const revealingElements = useRef([]);
 
   useEffect(() => {
@@ -28,6 +32,16 @@ function MyQuizzes() {
     try {
       const quizzesData = await getAllQuizzes();
       setQuizzes([...quizzesData]);
+      setOriginalQuizzes([...quizzesData]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchCategories = async () => {
+    try {
+      const categoriesData = await getAllCategories();
+      setCategories([...categoriesData]);
     } catch (error) {
       console.log(error);
     }
@@ -35,27 +49,53 @@ function MyQuizzes() {
 
   useEffect(() => {
     fetchData();
+    fetchCategories();
   }, []);
+
+  const handleFilter = (e) => {
+    const selectedValue = e.target.value;
+    setSelectedCategory(selectedValue);
+    if (selectedValue === "original") {
+      setQuizzes([...originalQuizzes]);
+    } else {
+      const filteredQuizzes = originalQuizzes.filter(
+        (quizz) => quizz.category === selectedValue
+      );
+      setQuizzes([...filteredQuizzes]);
+    }
+  };
 
   return (
     <div id="my-quizzes-content">
       <div className="component-content h-full flex flex-col justify-start md:w-[88vw] lg:w-[84vw] md:ml-[12vw] lg:ml-[16vw]">
-        <h1 className="mq-heading sm:my-[1vh] sm:mx-[2vw] lg:my-[5vh] lg:mx-[5vw] text-xl md:text-2xl lg:text-3xl mb-3 lg:pb-20">
-          Kolekcija kvizova
-        </h1>
+        <div className="flex items-center justify-between">
+          <h1 className="mq-heading sm:my-[1vh] sm:mx-[2vw] lg:my-[5vh] lg:mx-[5vw] text-xl md:text-2xl lg:text-3xl mb-3 lg:pb-20">
+            Kolekcija kvizova{" "}
+          </h1>
+          <select
+            value={selectedCategory}
+            onChange={handleFilter}
+            className="sm:my-[1vh] sm:mx-[2vw] lg:my-[5vh] lg:mx-[5vw] text-l md:text-xl lg:text-2xl p-3 rounded-md lg:mb-20 bg-[#e1bf57] text-white hover:cursor-pointer"
+          >
+            <option value="original">All categories</option>
+            {categories.map((category) => (
+              <option key={category._id} value={category._id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
         <ul className="grid lg:grid-cols-2 lg:grid-rows-3 gap-10 w-full md:grid-cols-1 sm:grid-cols-1 sm:mb-10">
-          {quizzes.map((quizz) => {
-            return (
-              <li key={quizz.id} className="flex justify-center align-center">
-                <Quizz
-                  id={quizz.id}
-                  name={quizz.name}
-                  description={quizz.description}
-                  imgSrc={quizz.image}
-                />
-              </li>
-            );
-          })}
+          {quizzes.map((quizz) => (
+            <li key={quizz.id} className="flex justify-center align-center">
+              <Quizz
+                id={quizz.id}
+                name={quizz.name}
+                description={quizz.description}
+                imgSrc={quizz.image}
+              />
+            </li>
+          ))}
         </ul>
         <div className="flex self-center bg-white rounded-xl my-5 gap-5">
           <button>
